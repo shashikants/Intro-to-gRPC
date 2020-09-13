dc50e36a8baa67b10faaa447a8c034d10a716e34
Shashikant Sejwar <shashikant.sejwar@quovantis.com>
2020-09-13 16:03:26 +0530

    Initial Commit - gRPC Client

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using gRPCServer;
+using System;
+using System.Threading.Tasks;
+
+namespace gRPCClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var request = new HelloRequest() { Name = "Shashikant" };
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(request);
+            Console.WriteLine(reply.Message);
+            Console.ReadLine();
+
+
+        }
+    }
+}
